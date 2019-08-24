bdf35067411264f0c9b9e16135c2b12e64ee01eb
n.mhamad <nzr.a.mhd@gmail.com>
2019-08-24 20:09:46 +0300

    with war

--- related/src/main/java/related/TestClass.java
+++ related/src/main/java/related/TestClass.java
@@ -1,5 +1,14 @@
 package related;
 
-public class TestClass {
+import java.io.File;
+import java.io.IOException;
 
+import org.apache.commons.io.FileUtils;
+
+public class TestClass {
+	private static final String PRINT_PATH = "D:\\temp\\printout.txt";
+
+	public void print(String message) throws IOException{
+		FileUtils.writeStringToFile(new File(PRINT_PATH), message);
+	}
 }
